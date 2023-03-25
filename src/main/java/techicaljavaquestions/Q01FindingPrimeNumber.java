3cbc4b8906e96031139261bb25a5c2349c05339c
İsmail Deniz <legaltechnologyinturkey@gmail.com>
2023-03-25 18:53:36 +0300

    I add the answer of the first question.

--- src/main/java/techicaljavaquestions/Q01FindingPrimeNumber.java
+++ src/main/java/techicaljavaquestions/Q01FindingPrimeNumber.java
@@ -1,8 +1,36 @@
 package techicaljavaquestions;
 
+import java.util.Scanner;
+
 public class Q01FindingPrimeNumber {
     /*
      * Create a method in Java to check if a given inputted number is prime number or not
      */
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please give me a number");
+        int num = input.nextInt();
+        System.out.println("Your number is = " + num);
+
+        int counter=0;
+
+        for (int i = 2; num > i ; i++) {
+
+            if(num%i==0){
+                counter++;
+            }
+        }
+
+        if(counter==0){
+
+            System.out.println("number is a prime number");
+        } else {
+            System.out.println("number is NOT a prime number");
+        }
+
+
+    }
 
 }
