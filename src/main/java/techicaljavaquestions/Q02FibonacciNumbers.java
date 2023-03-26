656aeef4c93c165dc948e6c2c5b675e9b8542630
İsmail Deniz <legaltechnologyinturkey@gmail.com>
2023-03-26 13:58:35 +0300

    I have just added the second question's answer

--- src/main/java/techicaljavaquestions/Q02FibonacciNumbers.java
+++ src/main/java/techicaljavaquestions/Q02FibonacciNumbers.java
@@ -1,5 +1,7 @@
 package techicaljavaquestions;
 
+import java.util.Scanner;
+
 public class Q02FibonacciNumbers {
 
     /*
@@ -9,5 +11,35 @@ these fibonacci numbers.
 Ex: if user enters 6 output will be 112358
 
  */
+    public static void main(String[] args) {
 
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("How many numbers do you see in the Fibonacci sequence?");
+    int num = input.nextInt();
+
+        System.out.println("Your Fibonacci sequence is: ");
+        int firstNum = 1;
+        int secondNum = 1;
+
+        if(num==1){
+            System.out.println(firstNum);
+        } else if (num==2) {
+            System.out.println(""+firstNum+secondNum);
+        } else if (num>2) {
+            System.out.print(""+firstNum+secondNum);
+            int fibNum=0;
+            for (int i = 2; i < num; i++) {
+
+                fibNum = firstNum + secondNum;
+                System.out.print(fibNum);
+                firstNum=secondNum;
+                secondNum=fibNum;
+
+            }
+
+        }
+
+
+    }
 }
