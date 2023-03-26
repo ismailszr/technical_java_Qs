51cadd930dbef85fa2bb5c60809ad9f42cbadee4
ismailszr <127437104+ismailszr@users.noreply.github.com>
2023-03-26 14:00:54 +0300

    Delete GizemClass.java

--- src/main/java/techicaljavaquestions/GizemClass.java
+++
@@ -1,9 +0,0 @@
-package techicaljavaquestions;
-
-import org.w3c.dom.ls.LSOutput;
-
-public class GizemClass {
-    public static void main(String[] args) {
-        System.out.println("Gizem Class");
-    }
-}
